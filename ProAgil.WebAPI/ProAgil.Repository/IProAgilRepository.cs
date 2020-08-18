76a555d5212bb3c3e720c71f66ab05dd31a9edba
Rafael Nunes <rafaelnunes_93@hotmail.com>
2020-08-18 10:09:19 -0300

    Erros encontrados e corrigidos

---
+++ ProAgil.WebAPI/ProAgil.Repository/IProAgilRepository.cs
@@ -0,0 +1,31 @@
+using ProAgil.Domain;
+using System.Threading.Tasks;
+
+namespace ProAgil.Repository
+{
+    public interface IProAgilRepository
+    {
+        //GERAL
+         void Add<T>(T entity) where T : class;    
+
+         void Update<T>(T entity) where T : class;  
+
+         void Delete<T>(T entity) where T : class; 
+
+         Task<bool> SaveChangesAsync();
+
+         //EVENTOS
+         Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includePalestrantes);
+
+         Task<Evento[]> GetAllEventoAsync(bool includePalestrantes);
+
+         Task<Evento> GetAllEventoAsyncById(int EventoId,bool includePalestrantes);
+
+        //PALESTRANTE
+         Task<Palestrante[]> GetAllPalestrantesAsyncByName(string name, bool includeEventos);
+
+         Task<Palestrante> GetPalestranteAsync(int palestranteId,bool includeEventos);
+
+
+    }
+}
